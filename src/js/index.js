1bc4b728a0136f371bc45b523de23752d7a4d316
Pavlo Syzonenko <pavlo.syzonenko@gmail.com>
2024-01-08 20:17:00 +0200

    feat: basics
    - create basic html layout for game board
    - create basic scss to style game squares
    - create some js coding to test game logic

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,26 @@
 import '../sass/main.scss';
+
+const board = document.querySelector('.game__board');
+
+const gameArr = [
+  [0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0],
+];
+
+board.addEventListener('click', (event) => {
+  console.log(event.target.dataset);
+
+  gameArr[event.target.dataset.row][event.target.dataset.column] = 1;
+
+  console.log(gameArr);
+
+  const element = document.querySelector(
+    `[data-row="${event.target.dataset.row}"][data-column="${event.target.dataset.column}"]`
+  );
+  element.style.backgroundColor = 'red';
+});
