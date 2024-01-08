32705accfc41a3fedba6da1dbde9094b40d566c6
Pavlo Syzonenko <pavlo.syzonenko@gmail.com>
2024-01-08 23:04:53 +0200

    fix: field activation logic feat: players turns refactor: move some logic in separate functions

--- src/js/index.js
+++ src/js/index.js
@@ -2,6 +2,7 @@ import '../sass/main.scss';
 
 const board = document.querySelector('.game__board');
 
+let currentPlayer = 'x';
 const gameArr = [
   [0, 0, 0, 0, 0, 0, 0],
   [0, 0, 0, 0, 0, 0, 0],
@@ -27,74 +28,76 @@ function renderBoard() {
         );
         square.style.backgroundColor = 'red';
       }
+      if (gameArr[row][column] === 3) {
+        const square = document.querySelector(
+          `[data-row="${row}"][data-column="${column}"]`
+        );
+        square.style.backgroundColor = 'blue';
+      }
     }
   }
 }
-renderBoard();
 
-board.addEventListener('click', (event) => {
-  // player missed a square
-  if (Object.keys(event.target.dataset).length === 0) return;
-  // square not 'active'
-  if (!event.target.classList.contains('active')) return;
-  // square is taken
-  if (event.target.dataset.occupied === 'true') return;
-  // if not taken mark as taken
-  else event.target.dataset.occupied = 'true';
+function updateCellValue(square) {
+  return square === 0 ? 1 : square;
+}
 
-  const row = Number(event.target.dataset.row);
-  const column = Number(event.target.dataset.column);
+function activateFields(row, column) {
+  const upperRow = row - 1 < 0 ? row : row - 1;
+  const lowerRow = row + 1 > 6 ? row : row + 1;
+  const leftColumn = column - 1 < 0 ? column : column - 1;
+  const rightColumn = column + 1 > 6 ? column : column + 1;
 
-  gameArr[row][column] = 2;
-  debugger;
-  if (row - 1 < 0 && column - 1 < 0) {
-    gameArr[row][column + 1] = 1;
-    gameArr[row + 1][column] = 1;
-    gameArr[row + 1][column + 1] = 1;
-  } else if (row - 1 < 0) {
-    gameArr[row][column - 1] = 1;
-    gameArr[row][column + 1] = 1;
-    gameArr[row + 1][column - 1] = 1;
-    gameArr[row + 1][column] = 1;
-    gameArr[row + 1][column + 1] = 1;
-  } else if (column + 1 > 7) {
-    gameArr[row - 1][column - 1] = 1;
-    gameArr[row - 1][column] = 1;
-    gameArr[row][column - 1] = 1;
-    gameArr[row + 1][column - 1] = 1;
-    gameArr[row + 1][column] = 1;
-  } else if (column - 1 < 0) {
-    gameArr[row - 1][column] = 1;
-    gameArr[row - 1][column + 1] = 1;
-    gameArr[row][column + 1] = 1;
-    gameArr[row + 1][column] = 1;
-    gameArr[row + 1][column + 1] = 1;
-  } else if (row + 1 > 7) {
-    gameArr[row - 1][column - 1] = 1;
-    gameArr[row - 1][column] = 1;
-    gameArr[row - 1][column + 1] = 1;
-    gameArr[row][column - 1] = 1;
-    gameArr[row][column + 1] = 1;
-  } else {
-    gameArr[row - 1][column - 1] = 1;
-    gameArr[row - 1][column] = 1;
-    gameArr[row - 1][column + 1] = 1;
-    gameArr[row][column - 1] = 1;
-    gameArr[row][column + 1] = 1;
-    gameArr[row + 1][column - 1] = 1;
-    gameArr[row + 1][column] = 1;
-    gameArr[row + 1][column + 1] = 1;
-  }
-  //   gameArr[row - 1][column - 1] = 1;
-  //   gameArr[row - 1][column] = 1;
-  //   gameArr[row - 1][column + 1] = 1;
-  //   gameArr[row][column - 1] = 1;
-  //   gameArr[row][column + 1] = 1;
-  //   gameArr[row + 1][column - 1] = 1;
-  //   gameArr[row + 1][column] = 1;
-  //   gameArr[row + 1][column + 1] = 1;
+  gameArr[upperRow][leftColumn] = updateCellValue(
+    gameArr[upperRow][leftColumn]
+  );
+  gameArr[upperRow][column] = updateCellValue(gameArr[upperRow][column]);
+  gameArr[upperRow][rightColumn] = updateCellValue(
+    gameArr[upperRow][rightColumn]
+  );
+  gameArr[row][leftColumn] = updateCellValue(gameArr[row][leftColumn]);
+  gameArr[row][rightColumn] = updateCellValue(gameArr[row][rightColumn]);
+  gameArr[lowerRow][leftColumn] = updateCellValue(
+    gameArr[lowerRow][leftColumn]
+  );
+  gameArr[lowerRow][column] = updateCellValue(gameArr[lowerRow][column]);
+  gameArr[lowerRow][rightColumn] = updateCellValue(
+    gameArr[lowerRow][rightColumn]
+  );
+}
 
-  console.log(gameArr);
+function playerMove(row, column) {
+  if (currentPlayer === 'x') gameArr[row][column] = 2;
+  if (currentPlayer === 'o') gameArr[row][column] = 3;
+}
 
+function togglePlayer() {
+  if (currentPlayer === 'x') currentPlayer = 'o';
+  else currentPlayer = 'x';
+}
+
+function init() {
   renderBoard();
-});
+
+  board.addEventListener('click', (event) => {
+    // player missed a square
+    if (Object.keys(event.target.dataset).length === 0) return;
+    // square not 'active'
+    if (!event.target.classList.contains('active')) return;
+    // square is taken
+    if (event.target.dataset.occupied === 'true') return;
+    // if not taken mark as taken
+    else event.target.dataset.occupied = 'true';
+
+    const row = Number(event.target.dataset.row);
+    const column = Number(event.target.dataset.column);
+
+    playerMove(row, column);
+    togglePlayer();
+
+    activateFields(row, column);
+
+    renderBoard();
+  });
+}
+init();
