fc617cad9ac94a8138b800dbee2770b0561bca86
Pavlo Syzonenko <pavlo.syzonenko@gmail.com>
2024-01-08 19:25:21 +0200

    initial commit
    - node package configuration
    - webpack configuration
    - directories hierarchy
    - readme instructions
    - gitignore

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # tictac7
+
 Tic-Tac-Toe game expanded to 7x7 field
+
+## Project Setup
+
+### Prerequisites
+
+- Node.js at least version 9.6.7 - You can download it here: [nodejs.org/en](https://nodejs.org/en)
+
+### Installation
+
+Run:
+
+```bash
+npm install
+```
+
+### Deployment
+
+To start it on localhost run:
+
+```bash
+npm start
+```
+
+To generate a 'dist' directory run:
+
+```bash
+npm run build
+```
---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  entry: './src/js/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: 'src/index.html',
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          'style-loader',
+          // Translates CSS into CommonJS
+          'css-loader',
+          // Compiles Sass to CSS
+          'sass-loader',
+        ],
+      },
+    ],
+  },
+  devServer: {
+    port: 5000,
+  },
+};
